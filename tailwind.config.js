20a0e7339c11642140517540390366ef964b9316
unknown <isfan.adheltyo@gmail.com>
2022-05-31 13:38:48 +0700

    portfolio-isfan-adheltyo

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '20px'
+    },
+    extend: {
+      colors: {
+        primary: '#f97316',
+        dark: '#0f172a'
+      },
+      screens: {
+        '2xl': '1500px'
+      }
+    },
+  },
+  plugins: [],
+}
